e6e81052d30918c92854384f49ce637e296f50d2
Nikola Ganchev <nikolamihailovg@gmail.com>
2022-10-18 21:49:30 +0300

    Add saving, retrieving images and perceptual hashing

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, redirect, flash, send_file
+from PIL import Image
+from flask import Flask, request, send_file
 
-from utils import is_valid_image, convert_to_webp
+from utils import is_valid_image, convert_to_webp, save, set_up_utils, save_webp_io, get_image_path, image_hash
 
 app = Flask(__name__)
+
+set_up_utils()
 
 
 @app.route('/image/valid', methods=['POST'])
@@ -16,7 +19,7 @@ def is_valid_image_endpoint():
         return app.response_class(
             status=415
         )
-    if file and is_valid_image(file):
+    if file and is_valid_image(Image.open(file)):
         return app.response_class(
             status=200
         )
@@ -37,13 +40,94 @@ def convert_to_webp_endpoint():
         return app.response_class(
             status=415
         )
-    img = convert_to_webp(file)
+    img = convert_to_webp(Image.open(file))
     if img is None:
         return app.response_class(
             status=415
         )
     else:
         return send_file(img, mimetype='image/webp')
+
+
+@app.route('/image/save', methods=['POST'])
+def save_image_endpoint():
+    if 'file' not in request.files:
+        return app.response_class(
+            status=415
+        )
+    file = request.files['file']
+    if file.filename == '':
+        return app.response_class(
+            status=415
+        )
+    name = save(Image.open(file))
+    if name is None:
+        return app.response_class(
+            status=415
+        )
+    else:
+        return app.response_class(
+            name,
+            status=201
+        )
+
+
+@app.route("/image/convert/save", methods=['POST'])
+def convert_and_save_image():
+    if 'file' not in request.files:
+        return app.response_class(
+            status=415
+        )
+    file = request.files['file']
+    if file.filename == '':
+        return app.response_class(
+            status=415
+        )
+    img = convert_to_webp(Image.open(file))
+    if img is None:
+        return app.response_class(
+            status=415
+        )
+    name = save_webp_io(img)
+    if name is None:
+        return app.response_class(
+            status=415
+        )
+    else:
+        return app.response_class(
+            name,
+            status=201
+        )
+
+
+@app.route("/image/<name>", methods=['GET'])
+def get_image_endpoint(name):
+    f = get_image_path(name)
+    if f is None:
+        return app.response_class(
+            status=404
+        )
+    else:
+        return send_file(f, mimetype='image/webp')
+
+
+@app.route("/image/hash", methods=['POST'])
+def phash_image_endpoint():
+    if 'file' not in request.files:
+        return app.response_class(
+            status=415
+        )
+    file = request.files['file']
+    if file.filename == '':
+        return app.response_class(
+            status=415
+        )
+    hash = image_hash(Image.open(file))
+    if hash is None:
+        return app.response_class(
+            status=415
+        )
+    return str(hash)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,13 +1,22 @@
+import os
+import pathlib
+import uuid
 from io import BytesIO
 
+import imagehash
 from werkzeug.datastructures import FileStorage
 from PIL import Image
 
+image_folder = "./image/"
 
-def is_valid_image(file: FileStorage):
+
+def set_up_utils():
+    if not os.path.isdir(image_folder):
+        os.mkdir(image_folder)
+
+
+def is_valid_image(im: Image):
     try:
-        im = Image.open(file)
-
         if im.format == 'GIF':
             return False
 
@@ -17,12 +26,40 @@ def is_valid_image(file: FileStorage):
         return False
 
 
-def convert_to_webp(file: FileStorage):
+def convert_to_webp(im: Image):
     try:
-        im = Image.open(file)
         img_io = BytesIO()
         im.save(img_io, format='webp', quality=70, optimise=True)
         img_io.seek(0)
         return img_io
+    except Exception as e:
+        return None
+
+
+def save_webp_io(img_io):
+    name = uuid.uuid4()
+    with open(f"{image_folder}{name}.webp", 'wb') as f:
+        f.write(img_io.getbuffer())
+    return name.__str__()
+
+
+def save(im: Image):
+    try:
+        name = uuid.uuid4()
+        im.save(pathlib.Path(f"{image_folder}{name}.{im.format.lower()}"))
+        return name.__str__()
+    except Exception as e:
+        return None
+
+
+def get_image_path(name):
+    if not os.path.isfile(f"{image_folder}{name}.webp"):
+        return None
+    return f"{image_folder}{name}.webp"
+
+
+def image_hash(im: Image):
+    try:
+        return bin(int(imagehash.average_hash(im).__str__(), base=16))[2:].zfill(64)
     except Exception as e:
         return None
